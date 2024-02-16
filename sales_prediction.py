970969f05daf24d72d0f7b2ab22953521832c92a
gaurav-ai22 <156395726+gaurav-ai22@users.noreply.github.com>
2024-02-16 13:40:16 +0530

    sales_prediction.py

--- sales_prediction.py
+++ sales_prediction.py
@@ -3,57 +3,46 @@
 # adversting x - 100000 -120000 - 140000 - 160000 - 220000
 # sales y - 100000 - 120000 - 140000 - 160000 - 220000
 
-import matplotlib.pyplot as plt 
+from statistics import mean
+import matplotlib.pyplot as plt
 
-ads = [100000,120000,140000,160000,220000]
-sales = [100000,120000,140000,160000,220000]
+year = [2013, 2014, 2015, 2016, 2017]
+adv = [100000, 120000, 140000, 160000, 220000]
+sales = [100000, 120000, 140000, 160000, 220000]
 
-mean_ads_x = sum(ads)//len(ads)
-mean_sales_y = sum(sales)//len(sales)
+years_mean = mean(year)
+advertising_mean = mean(adv)
+sales_mean = mean(sales)
 
-devi_x = []
-devi_y = []
+years_dev = []
+sales_dev = []
+product_dev = []
+square_dev_years = []
+square_dev_sales = []
 
+for i in range(len(adv)):
+    years_dev.append(year[i] - years_mean)
+    sales_dev.append(adv[i] - sales_mean)  # Corrected variable name
+    product_dev.append(years_dev[i] * sales_dev[i])
+    square_dev_years.append(years_dev[i] ** 2)
+    square_dev_sales.append(sales_dev[i] ** 2)
 
-for x in ads:
-    devi_x.append((x - mean_ads_x))
-for y in sales:
-    devi_y.append((y - mean_sales_y))
+sum_square_dev_years = sum(square_dev_years)
+sum_square_dev_sales = sum(square_dev_sales)
+sum_product_dev = sum(product_dev)
 
-print(f"Deviation of X:\n{devi_x}")
-print(f"Deviation of Y:\n{devi_y}")
+m = sum_product_dev / sum_square_dev_years
+b = sales_mean - (m * years_mean)
 
-product = []
-for i in range(len(devi_x)):
-    # produc = d[x] * d[y]
-    product.append((devi_x[i] * devi_y[i]))
+X = int(input("Enter the year: "))
+Y = m * X + b
+print(f'Predicted sales in {X} is {Y}')
 
-sqr_of_devi_of_x = []
-for x in devi_x:
-    sqr_of_devi_of_x.append((x**2))
-
-sum_of_product_of_devi = sum(product)
-sum_of_sqr_of_devi_of_x = sum(sqr_of_devi_of_x)
-
-m = sum_of_product_of_devi/sum_of_sqr_of_devi_of_x
-print(m)
-b = mean_sales_y - (m*mean_ads_x)
-print(b)
-
-last=devi_y[len(devi_y)-1]
-slast=devi_y[len(devi_y)-2]
-resTemp=sales[len(sales)-1]+(last-slast)
-print(resTemp)
-
-#  Y = m X + b
-# plotting
-# 2013,2014,2015,2016,2017,2018
-
-xplot = [100000,120000,140000,160000,220000,resTemp]
-yplot = [100000,120000,140000,160000,220000,resTemp]
-# data = [2,5,4]
-plt.plot(yplot,xplot,marker='*')
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("2018 SALES PRIDICTION")
+plt.scatter(year, adv, label='Actual Sales')  # Corrected variable name
+plt.plot(year, [m * x + b for x in year], color='red', label='Linear Regression')
+plt.xlabel('Year')
+plt.ylabel('Sales')
+plt.title('Linear Regression Example')
+plt.legend()
 plt.show()
+
